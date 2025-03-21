2321147cd660726d94a8d4bdf5d77c3647796559
David E Luna M <davidlunamontilla@gmail.com>
2025-03-21 05:12:55 -0500

    Update

---
+++ src/layers/pages/Main.svelte
@@ -0,0 +1,5 @@
+<script>
+    import Header from "../sections/Header.svelte";
+</script>
+
+<Header />
---
+++ src/layers/sections/Header.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+</script>
+
+<header class="header">
+    <nav class="header__nav">
+        <div class="header__logo"></div>
+        <div class="header__menu"></div>
+    </nav>
+</header>
