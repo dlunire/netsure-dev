55112e6378e6edb81df1220ef0c575e7c3502788
David E Luna M <davidlunamontilla@gmail.com>
2025-03-21 08:20:00 -0500

    Update

--- src/layers/sections/header/Header.svelte
+++ src/layers/sections/header/Header.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
+    import Logo from "../../icons/Logo.svelte";
     import "./header.scss";
+    import Menu from "./menu/Menu.svelte";
 </script>
 
 <header class="header">
-    <div class="header__inner">
-        <nav class="header__nav">
-            <div class="header__logo"></div>
-            <div class="header__menu"></div>
-        </nav>
-    </div>
+    <div class="header__blackbar"></div>
+
+    <nav class="header__nav header__nav--red">
+        <div class="header__logo">
+            <Logo />
+        </div>
+        <div class="header__menu header__menu--red">
+            <Menu />
+        </div>
+    </nav>
 </header>
---
+++ src/layers/sections/header/menu/Menu.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+    import type { SvelteComponent } from "svelte";
+    import "./menu.scss";
+    import IconContact from "../../../icons/IconContact.svelte";
+    import IconService from "../../../icons/IconService.svelte";
+    import IconAbout from "../../../icons/IconAbout.svelte";
+    import IconHome from "../../../icons/IconHome.svelte";
+
+    interface Menu {
+        label: string;
+        link: string;
+        icon?: typeof SvelteComponent;
+    }
+
+    let menu: Menu[] = [
+        {
+            label: "HOME",
+            link: "#home",
+            icon: IconHome as typeof SvelteComponent,
+        },
+        {
+            label: "NOSOTROS",
+            link: "#about",
+            icon: IconAbout as typeof SvelteComponent,
+        },
+        {
+            label: "SERVICIOS",
+            link: "#services",
+            icon: IconService as typeof SvelteComponent,
+        },
+        {
+            label: "CONTACTO",
+            link: "#contact",
+            icon: IconContact as typeof SvelteComponent,
+        },
+    ];
+</script>
+
+<ul class="menu">
+    {#each menu as item}
+        <li class="menu__item menu__item--white">
+            <a href={item.link} class="menu__link menu__link--white">
+                {#if item.icon}
+                    <svelte:component this={item.icon} />
+                {/if}
+                <span>{item.label}</span>
+            </a>
+        </li>
+    {/each}
+</ul>
