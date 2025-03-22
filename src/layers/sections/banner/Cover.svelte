51afb71d1e1d804461d9bf52e75e383d8225544d
David E Luna M <davidlunamontilla@gmail.com>
2025-03-22 12:31:03 -0500

    Update

--- src/layers/sections/banner/Cover.svelte
+++ src/layers/sections/banner/Cover.svelte
@@ -1,7 +1,18 @@
+<script>
+    import IconPortfolio from "../../icons/IconPortfolio.svelte";
+</script>
+
 <section class="banner" id="home">
     <img
         src="./images/norsire camion1.webp"
         alt="Imagen de fondo"
         class="banner__image"
     />
+
+    <div class="banner__inner">
+        <a href="./portfolio" class="button button--portfolio">
+            <IconPortfolio />
+            <span>Portafolio</span>
+        </a>
+    </div>
 </section>
