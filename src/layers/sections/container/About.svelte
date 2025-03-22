e3ae0648968051651233548f988c7d22f7435c79
David E Luna M <davidlunamontilla@gmail.com>
2025-03-22 05:15:35 -0500

    Update

---
+++ src/layers/sections/container/About.svelte
@@ -0,0 +1,54 @@
+<script>
+    import IconColombia from "../../icons/IconColombia.svelte";
+    import Logo from "../../icons/Logo.svelte";
+</script>
+
+<section class="section section--about" id="about">
+    <div class="section__inner section__inner--columns">
+        <article class="article section__article section__article--left">
+            <section class="article__section">
+                <div class="article__logo">
+                    <Logo />
+                </div>
+            </section>
+
+            <section class="article__section">
+                <p>
+                    Transporte, recolección y disposición final de residuos con
+                    características de peligrosidad.
+                </p>
+
+                <ul class="article__items">
+                    <li class="article__item">
+                        <span>Biológico e Infeccioso</span>
+                    </li>
+
+                    <li class="article__item">
+                        <span>Toxicidad</span>
+                    </li>
+
+                    <li class="article__item">
+                        <span>Corrosividad</span>
+                    </li>
+
+                    <li class="article__item">
+                        <span>Inflamabilidad</span>
+                    </li>
+
+                    <li class="article__item">
+                        <span>Explosividad</span>
+                    </li>
+                </ul>
+            </section>
+
+            <section class="article__section article__section--map">
+                <div>
+                    <IconColombia />
+                    <p>EMPRESA 100% NORTESANTANDEREANA</p>
+                </div>
+            </section>
+        </article>
+
+        <article class="section__article section__article--logo">dfads</article>
+    </div>
+</section>
--- src/layers/sections/container/Container.svelte
+++ src/layers/sections/container/Container.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
     import Cover from "../banner/Cover.svelte";
+    import About from "./About.svelte";
 </script>
 
 <section class="container">
     <Cover />
+    <About />
 </section>
