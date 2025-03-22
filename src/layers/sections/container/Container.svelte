be7bc98fe50950bbdcf42e3e6db5f88612685eb4
David E Luna M <davidlunamontilla@gmail.com>
2025-03-22 07:40:04 -0500

    Update

--- src/layers/sections/container/Container.svelte
+++ src/layers/sections/container/Container.svelte
@@ -2,10 +2,12 @@
     import Cover from "../banner/Cover.svelte";
     import About from "./About.svelte";
     import AboutUs from "./AboutUS.svelte";
+    import Services from "./Services.svelte";
 </script>
 
 <section class="container">
     <Cover />
     <About />
     <AboutUs />
+    <Services />
 </section>
---
+++ src/layers/sections/container/Services.svelte
@@ -0,0 +1,12 @@
+<script>
+    import IconService from "../../icons/IconService.svelte";
+</script>
+
+<section class="section section--services" id="services">
+    <div class="section__inner">
+        <h2 class="section__title">
+            <IconService />
+            <span>Nuestros servicios</span>
+        </h2>
+    </div>
+</section>
