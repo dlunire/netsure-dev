aeba6d8ed4cc51ba13aee8bbfd32ae77c76247c8
David E Luna M <davidlunamontilla@gmail.com>
2025-03-22 06:54:28 -0500

    Update

--- src/layers/sections/container/AboutUS.svelte
+++ src/layers/sections/container/AboutUS.svelte
@@ -9,6 +9,17 @@
             <IconAbout />
             <span>ACERCA DE NOSOTROS</span>
         </h2>
+
+        <h3 class="section__subtitle">¿Quiénes somos?</h3>
+        <p>
+            Somos una empresa que inició operaciones en el año 2020, interesada
+            en la situación ambiental del país, ofrecemos la implementación de
+            estrategias de residuos especiales y peligrosos.
+        </p>
+        <p>
+            NORSIRE se posiciona como una empresa líder en Norte de Santander
+            para la prestación de servicios medioambientales.
+        </p>
     </div>
 
     <Pattern />
