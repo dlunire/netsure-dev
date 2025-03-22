5fdf2f486ce907cf056ccfa72238ba0c3ae5f030
David E Luna M <davidlunamontilla@gmail.com>
2025-03-22 06:48:52 -0500

    Update

--- src/layers/sections/container/AboutUS.svelte
+++ src/layers/sections/container/AboutUS.svelte
@@ -1,6 +1,11 @@
+<script>
+    import IconAbout from "../../icons/IconAbout.svelte";
+</script>
+
 <section class="section section--about" id="about">
     <div class="section__inner">
         <h2 class="section__title">
+            <IconAbout />
             <span>ACERCA DE NOSOTROS</span>
         </h2>
     </div>
