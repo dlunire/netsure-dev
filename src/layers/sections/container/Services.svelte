2f364a0340b89f5ac5d7fb1fa9007ed017e876fc
David E Luna M <davidlunamontilla@gmail.com>
2025-03-22 07:55:31 -0500

    Update

--- src/layers/sections/container/Services.svelte
+++ src/layers/sections/container/Services.svelte
@@ -8,5 +8,51 @@
             <IconService />
             <span>Nuestros servicios</span>
         </h2>
+
+        <div class="section__services">
+            <article class="article article--services">
+                <header class="article__header">
+                    <img src="./images/" alt="Foto" />
+                </header>
+
+                <section class="article__container">
+                    <h3 class="article__title">Título</h3>
+                    <div class="article__content">Contenido normal</div>
+                </section>
+            </article>
+
+            <article class="article article--services">
+                <header class="article__header">
+                    <img src="./images/" alt="Foto" />
+                </header>
+
+                <section class="article__container">
+                    <h3 class="article__title">Título</h3>
+                    <div class="article__content">Contenido normal</div>
+                </section>
+            </article>
+
+            <article class="article article--services">
+                <header class="article__header">
+                    <img src="./images/" alt="Foto" />
+                </header>
+
+                <section class="article__container">
+                    <h3 class="article__title">Título</h3>
+                    <div class="article__content">Contenido normal</div>
+                </section>
+            </article>
+
+            <article class="article article--services">
+                <header class="article__header">
+                    <img src="./images/" alt="Foto" />
+                </header>
+
+                <section class="article__container">
+                    <h3 class="article__title">Título</h3>
+                    <div class="article__content">Contenido normal</div>
+                </section>
+            </article>
+        </div>
     </div>
 </section>
