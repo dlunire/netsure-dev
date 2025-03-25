9f391204ebcd167711e77f5ddc119fd4cc0d3638
David E Luna M <davidlunamontilla@gmail.com>
2025-03-24 23:01:36 -0500

    Update

--- src/layers/sections/footer/Footer.svelte
+++ src/layers/sections/footer/Footer.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import type { Menu } from "../../../lib/interface/menu";
+    import IconForm from "../../icons/IconForm.svelte";
+    import IconPortfolio from "../../icons/IconPortfolio.svelte";
     export let menu: Menu[] = [];
 </script>
 
@@ -34,7 +36,40 @@
                 <span>Enlaces externos</span>
             </h2>
 
-            <ul class="footer__menu"></ul>
+            <ul class="footer__menu">
+                <li class="footer__list">
+                    <a
+                        href="https://drive.google.com/file/d/18ovkKrJatJdvUdNifLplUBV2SsTjjcCV/view"
+                        target="_blank"
+                        class="footer__menu-link"
+                    >
+                        <IconPortfolio />
+                        <span>Portafolio</span>
+                    </a>
+                </li>
+
+                <li class="footer__list">
+                    <a
+                        href="https://drive.google.com/file/d/1jLmSbZ3H8TutQbmLx67Wbue0r4Q5-H_H/view"
+                        target="_blank"
+                        class="footer__menu-link"
+                    >
+                        <IconPortfolio />
+                        <span>Formato de propuesta económica</span>
+                    </a>
+                </li>
+
+                <li class="footer__list">
+                    <a
+                        href="https://docs.google.com/forms/d/e/1FAIpQLSd-E2T6CjR2leReUoF16Ku_klbpUykN2G0LJpG-iCnPkbP06w/viewform"
+                        target="_blank"
+                        class="footer__menu-link"
+                    >
+                        <IconForm />
+                        <span>Formulario de contacto</span>
+                    </a>
+                </li>
+            </ul>
         </div>
     </div>
 
